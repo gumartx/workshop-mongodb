36d8d130d5d4cc9bc6e66a3590fb7c89327e25b1
Gustavo Gomes <gustavo.gmartx@gmail.com>
2024-09-05 15:11:11 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/aws_springboot/mongodb/domain/Post.java
+++ src/main/java/com/aws_springboot/mongodb/domain/Post.java
@@ -6,6 +6,8 @@ import java.util.Objects;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import com.aws_springboot.mongodb.dto.AuthorDTO;
+
 @Document
 public class Post {
 
@@ -15,12 +17,12 @@ public class Post {
 	private String title;
 	private String body;
 
-	private User author;
+	private AuthorDTO author;
 
 	public Post() {
 	}
 	
-	public Post(String id, LocalDate date, String title, String body, User author) {
+	public Post(String id, LocalDate date, String title, String body, AuthorDTO author) {
 		this.id = id;
 		this.date = date;
 		this.title = title;
@@ -60,11 +62,11 @@ public class Post {
 		this.body = body;
 	}
 
-	public User getAuthor() {
+	public AuthorDTO getAuthor() {
 		return author;
 	}
 
-	public void setAuthor(User author) {
+	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
 
