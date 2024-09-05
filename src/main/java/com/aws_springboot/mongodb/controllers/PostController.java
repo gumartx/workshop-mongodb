ed062f07110c1e8ff21d0b24c15ef9db7cd555f5
Gustavo Gomes <gustavo.gmartx@gmail.com>
2024-09-05 17:47:23 -0300

    Consulta simples com @Query

--- src/main/java/com/aws_springboot/mongodb/controllers/PostController.java
+++ src/main/java/com/aws_springboot/mongodb/controllers/PostController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.aws_springboot.mongodb.controllers.util.URL;
 import com.aws_springboot.mongodb.domain.Post;
 import com.aws_springboot.mongodb.services.PostService;
 
@@ -28,6 +29,7 @@ public class PostController {
 	
 	@GetMapping(value = "/textsearch")
 	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+		text = URL.decodeParam(text);
 		List<Post> list = service.findByTitle(text);
 		return ResponseEntity.ok(list);
 	}
