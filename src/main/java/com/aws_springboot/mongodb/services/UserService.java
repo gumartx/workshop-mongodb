bafa9f92ca00814a698d96a98b78ffc02fd4c6b7
Gustavo Gomes <gustavo.gmartx@gmail.com>
2024-09-05 13:04:30 -0300

    Obtendo um usuário por id

--- src/main/java/com/aws_springboot/mongodb/services/UserService.java
+++ src/main/java/com/aws_springboot/mongodb/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.aws_springboot.mongodb.domain.User;
 import com.aws_springboot.mongodb.repositories.UserRepository;
+import com.aws_springboot.mongodb.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +17,11 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repository.findAll();
+	}
+	
+	public User findById(String id) {
+		User user = repository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+		return user;
 	}
 	
 }
---
+++ src/main/java/com/aws_springboot/mongodb/services/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,9 @@
+package com.aws_springboot.mongodb.services.exceptions;
+
+public class ObjectNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+}
