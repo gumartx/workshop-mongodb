4a087e9c8a17c49c6a6dd2e5f2348d076e41b711
Gustavo Gomes <gustavo.gmartx@gmail.com>
2024-09-05 13:22:00 -0300

    Inserção de usuário com POST

--- src/main/java/com/aws_springboot/mongodb/controllers/UserController.java
+++ src/main/java/com/aws_springboot/mongodb/controllers/UserController.java
@@ -1,13 +1,17 @@
 package com.aws_springboot.mongodb.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.aws_springboot.mongodb.domain.User;
 import com.aws_springboot.mongodb.dto.UserDTO;
@@ -33,4 +37,11 @@ public class UserController {
 		return ResponseEntity.ok(new UserDTO(obj));
 	}
 
+	@PostMapping
+	public ResponseEntity<UserDTO> insert(@RequestBody UserDTO userDTO) {
+		User obj = service.fromDTO(userDTO);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(new UserDTO(obj));
+	}
 }
--- src/main/java/com/aws_springboot/mongodb/services/UserService.java
+++ src/main/java/com/aws_springboot/mongodb/services/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.aws_springboot.mongodb.domain.User;
+import com.aws_springboot.mongodb.dto.UserDTO;
 import com.aws_springboot.mongodb.repositories.UserRepository;
 import com.aws_springboot.mongodb.services.exceptions.ObjectNotFoundException;
 
@@ -22,6 +23,15 @@ public class UserService {
 	public User findById(String id) {
 		User user = repository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 		return user;
+	}
+	
+	public User insert(User obj) {
+		obj = repository.insert(obj);
+		return obj;
+	}
+	
+	public User fromDTO(UserDTO user) {
+		return new User(user.getId(), user.getName(), user.getEmail());
 	}
 	
 }
