49d07c5694d4cba87d8fa64b3f8be815ecee78fd
Gustavo Gomes <gustavo.gmartx@gmail.com>
2024-09-05 14:56:36 -0300

    Criando entity Post com User aninhado

--- src/main/java/com/aws_springboot/mongodb/config/TestConfig.java
+++ src/main/java/com/aws_springboot/mongodb/config/TestConfig.java
@@ -1,12 +1,16 @@
 package com.aws_springboot.mongodb.config;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.aws_springboot.mongodb.domain.Post;
 import com.aws_springboot.mongodb.domain.User;
+import com.aws_springboot.mongodb.repositories.PostRepository;
 import com.aws_springboot.mongodb.repositories.UserRepository;
 
 @Configuration
@@ -14,17 +18,26 @@ public class TestConfig implements CommandLineRunner {
 
 	@Autowired
 	private UserRepository userRepository; 
+	@Autowired
+	private PostRepository postRepository;
 	
 	@Override
 	public void run(String... args) throws Exception {
 		
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
+		Post post1 = new Post(null, LocalDate.parse("21/03/2018", dtf), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
+		Post post2 = new Post(null, LocalDate.parse("23/03/2018", dtf), "Bom dia", "Acorder feliz hoje!", maria);
+		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
 }
