07af3b74ab843e3a110b3488bdaa6b610d92cd99
Gustavo Gomes <gustavo.gmartx@gmail.com>
2024-09-05 15:46:05 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/aws_springboot/mongodb/controllers/UserController.java
+++ src/main/java/com/aws_springboot/mongodb/controllers/UserController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.aws_springboot.mongodb.domain.Post;
 import com.aws_springboot.mongodb.domain.User;
 import com.aws_springboot.mongodb.dto.UserDTO;
 import com.aws_springboot.mongodb.services.UserService;
@@ -37,6 +38,12 @@ public class UserController {
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		User obj = service.findById(id);
 		return ResponseEntity.ok(new UserDTO(obj));
+	}
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok(obj.getPosts());
 	}
 
 	@PostMapping
