31144f066728b5309794f06d66f3f66fe52f925b
Gustavo Gomes <gustavo.gmartx@gmail.com>
2024-09-05 19:20:05 -0300

    Consulta com vários critérios

--- src/main/java/com/aws_springboot/mongodb/controllers/PostController.java
+++ src/main/java/com/aws_springboot/mongodb/controllers/PostController.java
@@ -1,5 +1,6 @@
 package com.aws_springboot.mongodb.controllers;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,4 +35,15 @@ public class PostController {
 		return ResponseEntity.ok(list);
 	}
 
+	@GetMapping(value = "/fullsearch")
+ 	public ResponseEntity<List<Post>> fullSearch(
+ 			@RequestParam(value="text", defaultValue="") String text,
+ 			@RequestParam(value="minDate", defaultValue="") String minDate,
+ 			@RequestParam(value="maxDate", defaultValue="") String maxDate) {
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		List<Post> list = service.fullSearch(text, min, max);
+		return ResponseEntity.ok().body(list);
+	}
 }
